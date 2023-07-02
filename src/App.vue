a1d5e878778b51a587c0b17244cdd40f4f3bac3f
Andrea Damiani <andrea.dam90@gmail.com>
2023-07-02 11:59:22 +0200

    Aggiunto Background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="h-[396px] w-[1584px] border bg-white shadow-lg"></div>
+    <div class="h-[396px] w-[1584px] bg-white bg-[url('/blurry-gradient-haikei.svg')] bg-center shadow-lg">
+        <h1 class="font-display text-5xl font-bold text-neutral-50 drop-shadow-md">Andrea Damiani</h1>
+        <div class="h-1 w-96 -skew-y-12 bg-neutral-50"></div>
+    </div>
 </template>
 
 <script setup></script>
