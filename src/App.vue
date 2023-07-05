80426eaa0686f01d090a9e708c374953756dec4a
Andrea Damiani <andrea.dam90@gmail.com>
2023-07-05 21:33:23 +0200

    lavoro ultimato

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
         <div class="mt-10 w-1/2">
             <div class="relative flex flex-col font-display font-bold text-neutral-50 drop-shadow-md">
                 <h1 class="text-6xl">Andrea Damiani</h1>
-                <h2 class="mt-3 text-4xl">Full Stack Web Developer</h2>
-                <div class="absolute -left-5 top-20 mt-20 h-1 w-[700px] -rotate-15 rounded-full bg-neutral-50"></div>
+                <h2 class="mt-3 text-4xl italic">Full Stack Web Developer</h2>
             </div>
-            <div class="absolute right-0 flex flex-col pr-24 drop-shadow-md">
-                <Icon icon="logos:vue" class="text-6xl" />
-                <Icon icon="logos:tailwindcss-icon" class="text-5xl" />
+
+            <div class="mt-10 flex w-fit -rotate-12 gap-8 border-t-8 pt-10 drop-shadow-md">
+                <Icon icon="vscode-icons:file-type-html" class="rotate-12 text-7xl" />
+                <Icon icon="vscode-icons:file-type-css" class="rotate-12 text-7xl" />
+                <Icon icon="logos:javascript" class="rotate-12 text-6xl" />
+                <Icon icon="logos:vue" class="rotate-12 text-6xl" />
+                <Icon icon="logos:tailwindcss-icon" class="rotate-12 text-5xl" />
+                <Icon icon="vscode-icons:file-type-php3" class="relative bottom-3 rotate-12 text-7xl" />
+                <Icon icon="logos:laravel" class="rotate-12 text-6xl" />
+                <Icon icon="vscode-icons:file-type-sql" class="rotate-12 text-6xl" />
             </div>
         </div>
     </div>
