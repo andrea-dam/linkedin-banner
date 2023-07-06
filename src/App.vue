06cc15bad98d5973d887d2dbf69a0456afaebbb4
Andrea Damiani <andrea.dam90@gmail.com>
2023-07-06 09:48:25 +0200

    pulizia codice

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex h-[396px] w-[1584px] justify-between bg-[url('/blurry-gradient-haikei.svg')] bg-center shadow-lg">
-        <div class="w-1/2"></div>
-        <div class="mt-10 w-1/2">
-            <div class="relative flex flex-col font-display font-bold text-neutral-50 drop-shadow-md">
-                <h1 class="text-6xl">Andrea Damiani</h1>
+        <div class="w-5/12"></div>
+        <div class="w-7/12 drop-shadow-md">
+            <div class="mt-8 flex flex-col font-display text-neutral-50">
+                <h1 class="text-6xl font-bold">Andrea Damiani</h1>
                 <h2 class="mt-3 text-4xl italic">Full Stack Web Developer</h2>
             </div>
 
-            <div class="mt-10 flex w-fit -rotate-12 gap-8 border-t-8 pt-10 drop-shadow-md">
+            <div class="mt-10 flex w-fit -rotate-12 gap-8 border-t-8 border-neutral-100 pt-10">
                 <Icon icon="vscode-icons:file-type-html" class="rotate-12 text-7xl" />
                 <Icon icon="vscode-icons:file-type-css" class="rotate-12 text-7xl" />
                 <Icon icon="logos:javascript" class="rotate-12 text-6xl" />
@@ -20,5 +20,3 @@
         </div>
     </div>
 </template>
-
-<script setup></script>
